b6e191eb66dd7925d020890da7bec581e89c6447
Enes Terzi <enestrz15@gmail.com>
2023-08-26 03:32:14 +0300

    Added appwrite connection, beautifull dnd and zustand

---
+++ lib/getTodosGroupedByColumn.ts
@@ -0,0 +1,10 @@
+import { databases } from "@/appwrite";
+
+export const getTodosGroupedByColumn = async () => {
+    const data = await databases.listDocuments(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_APPWRITE_TODOS_COLLECTION_ID!
+    );
+
+    console.log(data);
+};
